c6d251f8724d266665c17b5c9669fc3cedc90735
deepto <ddepp98@outlook.com>
2024-06-12 15:08:56 +0530

    completed take, added comments

--- programs/escrow/src/contexts/make.rs
+++ programs/escrow/src/contexts/make.rs
@@ -29,15 +29,15 @@ pub struct Make<'info> {
 
     // ATAs required - maker and vault
 
-    // Maker ATA - Tokens from Mint A will be transferred from Maker ATA to the vault
+    // Maker ATA for token A - Tokens from Mint A will be transferred from Maker ATA A to the vault
     // An ATA is derived from a wallet address(maker) and a mint address (mint_a)
     #[account( 
-        //no init because maker_ata should already exist 
+        //no init because maker_ata_a should already exist 
         mut, //mut since we transfer tokens from the ata 
         associated_token :: mint = mint_a, //ensuring ata is derived from Mint A
         associated_token :: authority = maker,//checks if ata is derived from maker address
     )]
-    pub maker_ata: Account<'info, TokenAccount>,
+    pub maker_ata_a: Account<'info, TokenAccount>,
 
     #[account(
         init,
@@ -54,7 +54,7 @@ pub struct Make<'info> {
 }
 
 impl<'info> Make<'info> {
-    // Initialize rules of escrow
+    // Initialize   escrow account with data
     pub fn init(&mut self, seed: u64, receive: u64, bumps: &MakeBumps) -> Result<()> {
         // set_inner helps us init the inner account of the Escrow. 
         self.escrow.set_inner(Escrow {
@@ -68,14 +68,14 @@ impl<'info> Make<'info> {
         Ok(())
     }
 
-    // Function for maker to deposit
+    // Function for maker to deposit from maker_ata_a to vault
     pub fn deposit(&mut self, deposit: u64) -> Result<()> {
         // For transferring sol, SystemProgram is target of CPI. Because System prog owns system account, which can transfer sol
         // For transferring SPL tokens,TokenProgram is the target. Because tokens are possesed by ATAs which are owned by Token Program
         let cpi_program = self.token_program.to_account_info();
 
         let cpi_accounts = Transfer {
-            from: self.maker_ata.to_account_info(),
+            from: self.maker_ata_a.to_account_info(),
             to: self.vault.to_account_info(),
             authority: self.maker.to_account_info(), //Auth required for ATA to ATA CPI. Auth is of the from acc
         };
@@ -86,3 +86,9 @@ impl<'info> Make<'info> {
         Ok(())
     }
 }
+
+// After deposit, the maker has deposited tokens   in the vault and said he wants x amount of mint b.
+// Now a taker has to accept the deal
+// transfer of mint b tokens from taker ata  to maker ata
+// then transfer tokens from vault to taker
+// then we close the state account
--- programs/escrow/src/contexts/take.rs
+++ programs/escrow/src/contexts/take.rs
@@ -1,30 +1,37 @@
 use anchor_lang::{ prelude::*, solana_program::instruction };
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{ transfer, Mint, Token, TokenAccount, Transfer },
+    token::{ close_account, transfer, CloseAccount, Mint, Token, TokenAccount, Transfer },
 };
 
 use crate::state::Escrow;
+
+// Taker - First, Vault to taker. Then, taker_ata_b to maker_ata_b.
 
 // Taker accepts terms, we send tokens from taker's ata for mint b to maker's ata for mint b
 // Then we send tokens in vault to taker's ata on mint a
 #[derive(Accounts)]
 #[instruction(seed:u64)] //seed will be passed from client, we use instruction to use it within context
 pub struct Take<'info> {
-    #[account(mut)] //mut because maker pays for init - sol change
-    pub taker: Signer<'info>, //person initializing escrow and paying fees and signing SPL tokens, has to be a signer
+    #[account(mut)] // taker pays initialization fees, so is mutable
+    pub taker: Signer<'info>, //taker signs the txn
 
     pub maker: SystemAccount<'info>,
 
-    // Mint Accounts
+    // Mint Accounts mint a : mint add of maker, mint_b : of taker
     pub mint_a: Account<'info, Mint>, //derive PDAs. to be stored in escrow
     pub mint_b: Account<'info, Mint>, //to be stored in escrow
 
     #[account(
+        mut, //since we'll close it after the txn
         seeds = [b"escrow", escrow.maker.key.as_ref, escrow.seed.to_le_bytes().as_ref()], // by passing seed, we allow multiple escrows per user. seed is fetched from instruction macro here
         bump = escrow.bump,
-        has_one = mint_a, //states that escrow needs to have mint acc with same addressmas mint a here
-        has_one = mint_b
+
+        //Checks if these items are already stored in the account struct
+        has_one = mint_a, //states that escrow needs to have mint acc with same address  as mint a here
+        has_one = mint_b,
+        has_one = maker,
+        close = maker
     )]
     pub escrow: Account<'info, Escrow>,
 
@@ -36,22 +43,16 @@ pub struct Take<'info> {
     )]
     pub vault: Account<'info, TokenAccount>,
 
+    // taker_ata_a - receives maker's tokens from mint_a
     #[account(
-        init_if_needed,
-        payer = taker,
-        associated_token::mint = mint_b,
-        associated_token::authority = maker //checks if ata is derived from maker address
-    )]
-    pub maker_ata: Account<'info, TokenAccount>,
-
-    #[account(
-        init_if_needed,
+        init_if_needed, // since we don't know if the acc already exists
         payer = taker,
         associated_token::mint = mint_a,
-        associated_token::authority = taker
+        associated_token::authority = taker // taker is auth (wallet add for taker_ata_a)
     )]
     pub taker_ata_a: Account<'info, TokenAccount>,
 
+    // taker_ata_b - sends taker's tokens from mint_b
     #[account(
         mut,
         associated_token::mint=mint_b,
@@ -59,12 +60,24 @@ pub struct Take<'info> {
     )]
     pub taker_ata_b: Account<'info, TokenAccount>,
 
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker //checks if ata is derived from maker address
+    )]
+    pub maker_ata_b: Account<'info, TokenAccount>,
+
     // Programs - system prog, token prog, associated token prog
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>, //ownership will be transferred to token program
     pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
+// Take functionalities:
+// 1. Deposit from taker ata b to maker ata b
+// 2 .Withdraw from vault, send to taker. mint address : mint_a
+// 3. Close vault
 impl<'info> Take<'info> {
     // pub fn take(&mut self, seed: u64) -> Result<()> {
     //     self.escrow.send = send;
@@ -79,25 +92,26 @@ impl<'info> Take<'info> {
     //     Ok(())
     // }
 
-    pub fn transfer(&mut self) -> Result<()> {
+    pub fn deposit(&mut self) -> Result<()> {
         // token program coz spl
         let cpi_program = self.token_program.to_account_info();
 
-        //taker_ata_b to maker_ata
+        //taker_ata_b to maker_ata b
         let cpi_accounts = Transfer {
             from: self.taker_ata_b.to_account_info(),
-            to: self.maker_ata.to_account_info(),
+            to: self.maker_ata_b.to_account_info(),
             authority: self.taker.to_account_info(),
         };
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        transfer(cpi_ctx, self.escrow.receive)?;
+        transfer(cpi_ctx, self.escrow.receive)?; //receive is defiend in escrow
 
         Ok(())
     }
 
-    // vault to taker_ata_a
-    pub fn withdraw (&mut self) -> Result<()> {
+    // transfer from vault to taker_ata_a
+    pub fn withdraw_and_close(&mut self) -> Result<()> {
+        // 1. Withdraw
         // token program coz spl
         let cpi_program = self.token_program.to_account_info();
 
@@ -108,9 +122,28 @@ impl<'info> Take<'info> {
             authority: self.escrow.to_account_info(),
         };
 
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        transfer(cpi_ctx, self.escrow.receive)?;
+        // Signer seeds - We need the authority of the program , program signs on behalf of PDA
+        let signer_seeds = [
+            &[
+                b"escrow",
+                self.maker.to_account_info().key.as_ref(),
+                &self.escrow.seed.to_be_bytes()[..],
+                &[self.escrow.bump],
+            ],
+        ];
 
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+        transfer(cpi_ctx, self.vault.amount)?; //we transfer all tokens in vault to taker
+
+        // 2. Close Account
+        let accounts = CloseAccount {
+            account: self.vault.to_account_info(), //account we're closing
+            destination: self.taker.to_account_info(), //where rent goes to
+            authority: self.escrow.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, accounts, &signer_seeds);
+        close_account(cpi_ctx);
         Ok(())
     }
 }
